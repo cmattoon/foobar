a9a556645a40c13cd713f5e744178360a2e05c70
Curtis Mattoon <cmattoon@cmattoon.com>
2017-08-05 00:28:35 -0400

    ???

--- doomsday_fuel/test.py
+++ doomsday_fuel/test.py
@@ -3,7 +3,19 @@ import unittest
 from ddt import ddt, data, unpack
 
 
+@ddt
 class SolutionTestCase(unittest.TestCase):
+
+    # @data(
+    #     (# Case 1
+    #         [[1,2,3],[4,5,6],[7,8,9],[10,11,12]],
+    #         [[1,2],[1,2],[3,4]],
+    #         [[12, 18], [27, 42], [42, 66], [57, 90]]
+    #     ),
+    # )
+    # @unpack
+    # def test_matmul(self, m1, m2, m3):
+    #     self.assertEqual(m3, solution.matmul(m1, m2))
 
     @data(
         (# Case 1
@@ -32,6 +44,6 @@ class SolutionTestCase(unittest.TestCase):
     def test_answer(self, m, exp):
         self.assertEquals(exp, solution.answer(m))
 
-
+        
 if __name__ == '__main__':
     unittest.main()
